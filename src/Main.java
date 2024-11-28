c03a735807e96c66546a8b0a8c90ddc52c1cfbe5
Jorge Salazar <jorgehernando.salazar@ikasle.egibide.org>
2024-11-28 21:23:42 +0100

    Commit de Jorge

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static String[] actualizar(String[] tareas, String descripcion, String tarea){
+        for (int i = 0; i < tareas.length; i++) {
+            if (tareas[i].equals(tarea)){
+                tareas[i] = tarea;
+            }
         }
+        return tareas;
+    }
+
+    public static void main(String[] args) throws IOException {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Ingrese la tarea a actualiziar: ");
+        String tarea = br.readLine();
+        System.out.println("Ingrese el nuevo valor: ");
+        String descripcion = br.readLine();
+        actualizar(array, descripcion, tarea);
     }
 }
